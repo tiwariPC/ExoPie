42af9871bfe859161132f50e3eaaedb836c5c134
Raman Khurana <ramankhurana1986@gmail.com>
2019-07-03 16:34:53 +0200

    added temporary shell script to mrge data files, has to be replaced by better solution

---
+++ analysistools/mergetools/merge_data_tmp.sh
@@ -0,0 +1,4 @@
+#inputpath=/eos/cms/store/user/khurana//ExoPieInputs/MergedSkims/Merged_tDM_06052019/
+inputpath=/tmp/khurana/Merged_tDM_06052019/
+hadd Merged_SingleElectron-Run2016.root ${inputpath}/Merged_SingleElectron*
+#hadd Merged_SingleMu-Run2016.root ${inputpath}/Merged_SingleMu*
